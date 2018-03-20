2d37f834d80e15388f217ed4da398b1864839798
SilviaAsmat <silvia.asmat@g.austincc.edu>
2018-03-19 20:31:14 -0500

    Removed color functions

--- SeinfeldQuotes/src/lambda_function.py
+++ SeinfeldQuotes/src/lambda_function.py
@@ -64,65 +64,12 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
+    speech_output = "Thank you for using Seinfeld Quotes " \
                     "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
-
-
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
-
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
 
 
 # --------------- Events ------------------
@@ -158,9 +105,9 @@ def on_intent(intent_request, session):
     if intent_name == "GetQuoteFromNameIntent":
         return handle_get_quote_from_name_intent(intent)
     elif intent_name == "GetRandomQuoteIntent":
-        return get_color_from_session(intent, session)
+        return handle_get_random_quote_intent(intent)
     elif intent_name == "GetQuoteFromSeasonIntent":
-        return get_color_from_session(intent, session)
+        return handle_get_quote_from_season_intent(intent)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
@@ -198,13 +145,20 @@ def handle_get_quote_from_season_intent(intent):
         card_title, quote, reprompt_text, should_end_session))
 
 # TODO get quote from Database
+
+
 def get_quote_from_name(name):
     return 'These pretzels are making me thirsty'
 
+
 def get_random_quote():
     return 'You kept making all the stops, well people kept ringing the bell'
+
+
 def get_quote_from_season(number):
     return 'You double chipped the chip'
+
+
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
 
